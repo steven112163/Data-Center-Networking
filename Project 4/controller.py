dcefa3077d1238b3ff4a1cce5f4bb62284d1f50a
steven112163 <steven112163@gmail.com>
2021-05-04 12:05:03 +0800

    Use networkx and add links in config

--- Project 4/controller.py
+++ Project 4/controller.py
@@ -1,8 +1,11 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet, ethernet, ether_types
+from ryu.lib import hub
+from ryu.lib.packet import packet, ethernet, ether_types, lldp
+from networkx import NetworkXNoPath
+import networkx as nx
 import json
 
 
@@ -11,13 +14,17 @@ class SimpleSwitch13(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
-        self.mac_to_tenant = {}
+        self.data_paths = {}
+        self.data_path_to_ports = {}
+        self.network = nx.DiGraph()
+        self.lldp_thread = hub.spawn(self.lldp_sender)
+        self.mac_to_group = {}
         with open('./utils/config.json') as f:
             configuration = json.load(f)
-            for key, macs in configuration.items():
+            for key, macs in configuration['groups'].items():
                 for mac in macs:
-                    self.mac_to_tenant[mac] = key
+                    self.mac_to_group[mac] = key
+            self.mac_to_leaf = configuration['links']
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -37,6 +44,15 @@ class SimpleSwitch13(app_manager.RyuApp):
                                           of_proto.OFPCML_NO_BUFFER)]
         self.add_flow(data_path, 0, match, actions)
 
+        # Install LLDP flow entry
+        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_LLDP)
+        actions = [parser.OFPActionOutput(of_proto.OFPP_CONTROLLER,
+                                          of_proto.OFPCML_NO_BUFFER)]
+        self.add_flow(data_path, 0, match, actions)
+
+        # Request all ports' description
+        self.request_ports(data_path)
+
     def add_flow(self, data_path, priority, match, actions, buffer_id=None):
         of_proto = data_path.ofproto
         parser = data_path.ofproto_parser
@@ -52,6 +68,87 @@ class SimpleSwitch13(app_manager.RyuApp):
                                     match=match, instructions=inst)
         data_path.send_msg(mod)
 
+    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def state_change_handler(self, ev):
+        data_path = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            if data_path.id not in self.data_paths:
+                self.logger.debug('register data path: %016x', data_path.id)
+                self.data_paths[data_path.id] = data_path
+                self.data_path_to_ports[data_path.id] = []
+        elif ev.state == DEAD_DISPATCHER:
+            if data_path.id in self.data_paths:
+                self.logger.debug('unregister data path: %016x', data_path.id)
+                del self.data_paths[data_path.id]
+                del self.data_path_to_ports[data_path.id]
+                data_path_id = str(data_path.id)
+                if data_path_id in self.network:
+                    self.network.remove_node(data_path_id)
+
+    @staticmethod
+    def request_ports(data_path):
+        """
+        Send port description request to the switch
+        :param data_path: the target switch
+        :return: None
+        """
+        of_proto = data_path.ofproto
+        parser = data_path.ofproto_parser
+
+        req = parser.OFPPortDescStatsRequest(data_path, 0, of_proto.OFPP_ANY)
+        data_path.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def port_desc_reply_handler(self, ev):
+        """
+        Collect all ports belong to the data path
+        :param ev: received event
+        :return: None
+        """
+        msg = ev.msg
+        body = msg.body
+        data_path = msg.datapath
+        of_proto = data_path.ofproto
+
+        for stat in body:
+            if stat.port_no < of_proto.OFPP_MAX:
+                self.data_path_to_ports[data_path.id].append((stat.port_no, stat.hw_addr))
+
+    def lldp_sender(self):
+        """
+        Send LLDP every 5 seconds
+        :return: None
+        """
+        while True:
+            for data_path_id, data_path in self.data_paths.items():
+                if data_path_id in self.data_path_to_ports:
+                    list_of_ports = self.data_path_to_ports[data_path_id]
+                    for port_no, hw_addr in list_of_ports:
+                        self.send_lldp(data_path, port_no, hw_addr)
+            hub.sleep(5)
+
+    @staticmethod
+    def send_lldp(data_path, port_no, hw_addr):
+        ofp = data_path.ofproto
+        pkt = packet.Packet()
+        pkt.add_protocol(
+            ethernet.ethernet(ethertype=ether_types.ETH_TYPE_LLDP, src=hw_addr, dst=lldp.LLDP_MAC_NEAREST_BRIDGE))
+
+        tlv_chassis_id = lldp.ChassisID(subtype=lldp.ChassisID.SUB_LOCALLY_ASSIGNED, chassis_id=str(data_path.id))
+        tlv_port_id = lldp.PortID(subtype=lldp.PortID.SUB_LOCALLY_ASSIGNED, port_id=str(port_no))
+        tlv_ttl = lldp.TTL(ttl=10)
+        tlv_end = lldp.End()
+        tlvs = (tlv_chassis_id, tlv_port_id, tlv_ttl, tlv_end)
+        pkt.add_protocol(lldp.lldp(tlvs))
+        pkt.serialize()
+
+        data = pkt.data
+        parser = data_path.ofproto_parser
+        actions = [parser.OFPActionOutput(port=port_no)]
+        out = parser.OFPPacketOut(datapath=data_path, buffer_id=ofp.OFP_NO_BUFFER, in_port=ofp.OFPP_CONTROLLER,
+                                  actions=actions, data=data)
+        data_path.send_msg(out)
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
         # If you hit this you might want to increase
@@ -66,22 +163,43 @@ class SimpleSwitch13(app_manager.RyuApp):
         in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        eth = pkt.get_protocol(ethernet.ethernet)
+        if not eth:
+            return
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
-            return
+            lldp_pkt = pkt.get_protocol(lldp.lldp)
+            if lldp_pkt:
+                self.lldp_pkt_handler(data_path, in_port, lldp_pkt)
+        else:
+            self.normal_pkt_handler(data_path, msg, of_proto, parser, in_port, eth)
+
+    def normal_pkt_handler(self, data_path, msg, of_proto, parser, in_port, eth):
+        """
+        Normal packet handler
+        :return: None
+        """
         dst = eth.dst
         src = eth.src
+        data_path_id = str(data_path.id)
 
-        data_path_id = format(data_path.id, "d").zfill(16)
-        self.mac_to_port.setdefault(data_path_id, {})
+        if src not in self.network:
+            self.network.add_node(src)
+            self.network.add_edge(data_path_id, src, port=in_port)
+            self.network.add_edge(src, data_path_id)
 
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[data_path_id][src] = in_port
+        if src in self.mac_to_group and dst in self.mac_to_group:
+            if self.mac_to_group[src] != self.mac_to_group[dst]:
+                # Src and Dst belong to different groups
+                return
 
-        if dst in self.mac_to_port[data_path_id]:
-            out_port = self.mac_to_port[data_path_id][dst]
+        if dst in self.network:
+            try:
+                path = nx.shortest_path(self.network, src, dst)
+                next_hop = path[path.index(data_path_id) + 1]
+                out_port = self.network[data_path_id][next_hop]['port']
+            except NetworkXNoPath:
+                out_port = of_proto.OFPP_FLOOD
         else:
             out_port = of_proto.OFPP_FLOOD
 
@@ -104,3 +222,21 @@ class SimpleSwitch13(app_manager.RyuApp):
         out = parser.OFPPacketOut(datapath=data_path, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
         data_path.send_msg(out)
+
+    def lldp_pkt_handler(self, data_path, in_port, lldp_pkt):
+        """
+        LLDP packet handler
+        :return: None
+        """
+        sender_id = lldp_pkt.tlvs[0].chassis_id
+        sender_port = lldp_pkt.tlvs[1].port_id
+        receiver_id = str(data_path.id)
+        receiver_port = in_port
+
+        if sender_id not in self.network:
+            self.network.add_node(sender_id)
+        if receiver_id not in self.network:
+            self.network.add_node(receiver_id)
+
+        self.network.add_edge(sender_id, receiver_id, port=sender_port)
+        self.network.add_edge(receiver_id, sender_id, port=receiver_port)
